b69f6b512bbef29a965e51ba7cf5f6e854132bdf
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-10-17 15:38:29 -0400

    added api, data, and people directories. added server and peopleModel files and their respective testing files. added 2 db3 data files. initialized index.js and knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,36 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/people.db3'
+    },
+
+    migrations: {
+      directory: './data/migrations',
+    },
+
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './data/test.db3',
+    },
+
+    migrations: {
+      directory: './data/migrations',
+    },
+
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+};
